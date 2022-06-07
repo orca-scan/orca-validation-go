e1eafe629a1b47b69aec1fce5c04c633f16e830a
wole-kolawole <101117076+wole-kolawole@users.noreply.github.com>
2022-06-07 23:47:22 +0100

    Modified code to return 200 with empty body to match documentation on the website

--- server.go
+++ server.go
@@ -53,8 +53,8 @@ func validationHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// return HTTP Status 204 (No Content)
-	w.WriteHeader(204)
+	// return HTTP Status 200 with no body
+	w.Write([]byte(""))
 }
 
 func main() {
